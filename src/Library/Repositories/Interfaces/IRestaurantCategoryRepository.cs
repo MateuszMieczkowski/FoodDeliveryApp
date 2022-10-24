2f1265881e78e66c37bc67b81ad30135fe7513dd
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-10-24 22:42:04 +0200

    added restaurantCategoryRepository interface and implementation

---
+++ src/Library/Repositories/Interfaces/IRestaurantCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Library.Entities;
+
+namespace Library.Repositories.Interfaces;
+
+public interface IRestaurantCategoryRepository
+{
+    public IEnumerable<RestaurantCategory> Categories { get; set; }
+    public Task AddCategoryAsync(RestaurantCategory category);
+    public Task DeleteCategoryAsync(RestaurantCategory category);
+    public Task<RestaurantCategory?> GetRestaurantCategory(string name);
+}
---
+++ src/Library/Repositories/RestaurantCategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using Library.DataPersistence;
+using Library.Entities;
+using Library.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Repositories;
+
+public class RestaurantCategoryRepository : IRestaurantCategoryRepository
+{
+    private readonly ApplicationDbContext _dbcontext;
+
+    public RestaurantCategoryRepository(ApplicationDbContext dbcontext)
+    {
+        _dbcontext = dbcontext;
+        Categories = dbcontext.RestaurantCategories.Include(r => r.Restaurants);
+    }
+
+    public IEnumerable<RestaurantCategory> Categories { get; set; }
+
+    public async Task AddCategoryAsync(RestaurantCategory category)
+    {
+        if(_dbcontext.RestaurantCategories.Any(r=>r.Name == category.Name))
+        {
+            return;
+        }
+        await _dbcontext.RestaurantCategories.AddAsync(category);
+        await _dbcontext.SaveChangesAsync();
+    }
+
+    public async Task DeleteCategoryAsync(RestaurantCategory category)
+    {
+        _dbcontext.RestaurantCategories.Remove(category);
+        await _dbcontext.SaveChangesAsync();
+    }
+
+    public async Task<RestaurantCategory?> GetRestaurantCategory(string name)
+    {
+        return await _dbcontext.RestaurantCategories.SingleOrDefaultAsync(r => r.Name == name);
+    }
+}
