ba3d1005bae173e624918752d538411d47b26e29
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-10-24 19:26:54 +0200

    Renamed restaurantController to restaurantsController and added delete action methods

--- src/Web/Api/Controllers/RestaurantsController.cs
+++ src/Web/Api/Controllers/RestaurantsController.cs
@@ -1,17 +1,15 @@
-﻿using Library.DataPersistence;
-using Library.Entities;
-using Library.Repositories.Interfaces;
+﻿using Library.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Web.Api.Models;
 
 namespace Web.Api.Controllers
 {
     [ApiController]
-    [Route("/api/restaurant")]
-    public class RestaurantController : ControllerBase
+    [Route("/api/restaurants")]
+    public class RestaurantsController : ControllerBase
     {
         private readonly IRestaurantRepository _restaurantRepository;
-        public RestaurantController(IRestaurantRepository restaurantRepository)
+        public RestaurantsController(IRestaurantRepository restaurantRepository)
         {
             _restaurantRepository = restaurantRepository;
         }
@@ -28,13 +26,26 @@ namespace Web.Api.Controllers
         {
             var restaurant = await _restaurantRepository.GetRestaurantAsync(id);
 
-            if(restaurant is null)
+            if (restaurant is null)
             {
                 return NotFound();
             }
 
             var restaurantDto = restaurant.ToDto();
             return Ok(restaurantDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<RestaurantDto>> DeleteRestaurant(int id)
+        {
+            var restaurant = await _restaurantRepository.GetRestaurantAsync(id);
+            if(restaurant is null)
+            {
+                return NotFound();
+            }
+            await _restaurantRepository.DeleteRestaurantAsync(restaurant);
+
+            return NoContent();
         }
 
     }
