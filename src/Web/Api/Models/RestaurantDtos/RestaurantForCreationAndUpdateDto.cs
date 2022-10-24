18ce50811e67ce25abc83bd4160260deb1801ece
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-10-24 22:58:36 +0200

    renamed dto

---
+++ src/Web/Api/Models/RestaurantDtos/RestaurantForCreationAndUpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using Library.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Api.Models.RestaurantDtos
+{
+    public class RestaurantForCreationAndUpdateDto
+    {
+        [Required]
+        [MaxLength(70)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(100)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public RestaurantCategory RestaurantCategory { get; set; } = default!;
+
+        public ICollection<Product>? Products { get; set; }
+
+        public ICollection<Order>? Orders { get; set; }
+
+        public ICollection<RestaurantReview>? Reviews { get; set; }
+    }
+}
