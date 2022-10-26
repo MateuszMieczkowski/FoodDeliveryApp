9b1f2907faf55d7820bbd2a5421ef1ebf33c6c42
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-10-26 22:53:44 +0200

    add category controller

---
+++ src/Web/Api/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Library.Entities;
+using Library.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Web.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/category")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IRestaurantCategoryRepository _restaurantCategoryRepository;
+
+        public CategoryController(IRestaurantCategoryRepository restaurantCategoryRepository)
+        {
+            _restaurantCategoryRepository = restaurantCategoryRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantCategory>> GetCategories()
+        {
+            return Ok(_restaurantCategoryRepository.Categories);
+        }
+    }
+}
