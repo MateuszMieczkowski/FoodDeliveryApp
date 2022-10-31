0082cc8c76025831764e7a6f506442129818af67
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-10-31 19:25:14 +0100

    add reviews controller

---
+++ src/Web/Api/Controllers/ReviewsController.cs
@@ -0,0 +1,55 @@
+﻿using Library.Entities;
+using Library.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Web.Api.Models.RestaurantDtos;
+
+namespace Web.Api.Controllers;
+
+[ApiController]
+[Route("/api/restaurant/{restaurantId}/review")]
+public class ReviewsController : ControllerBase
+{
+    private readonly IRestaurantRepository _restaurantRepository;
+
+    public ReviewsController(IRestaurantRepository restaurantRepository)
+    {
+        _restaurantRepository = restaurantRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<RestaurantReviewDto>>> GetReviews(int restaurantId)
+    {
+        var restaurant = await _restaurantRepository.GetRestaurantAsync(restaurantId);
+        if(restaurant is null)
+        {
+            return NotFound();
+        }
+        var reviews = restaurant.Reviews;
+        return Ok(reviews);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddReview(int restaurantId, RestaurantReviewForUpdateDto reviewDto)
+    {
+        var restaurant = await _restaurantRepository.GetRestaurantAsync(restaurantId);
+        if (restaurant is null)
+        {
+            return NotFound();
+        }
+
+        var newReview = new RestaurantReview()
+        {
+            Title = reviewDto.Title,
+            Description = reviewDto.Description,
+            Rating = reviewDto.Rating
+        };
+        restaurant.Reviews.Add(newReview);
+        await _restaurantRepository.SaveChangesAsync();
+
+        return Ok();
+    }
+
+
+}
+
+
