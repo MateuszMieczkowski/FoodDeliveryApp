a6a242bb869a3e73d1c042a9b6cc0cb5e5c658bd
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-10-31 19:26:05 +0100

    ui changes

---
+++ src/Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Web.Api.Models.RestaurantDtos;
+
+namespace Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<RestaurantDto>? Restaurants { get; set; }
+
+        public HomeViewModel(IEnumerable<RestaurantDto>? restaurants)
+        {
+            Restaurants = restaurants;
+        }
+    }
+}
