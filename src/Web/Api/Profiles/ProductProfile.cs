3f009b8d41499ad45bc8bbf1ba7bcccb112309b7
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-12-01 17:10:39 +0100

    add new profile

--- src/Web/Api/Profiles/ProductProfile.cs
+++ src/Web/Api/Profiles/ProductProfile.cs
@@ -9,6 +9,8 @@ public class ProductProfile : Profile
 	public ProductProfile()
 	{
 		CreateMap<Product, ProductDto>().ReverseMap().PreserveReferences();
+		CreateMap<Product, ProductForUpdateDto>().ReverseMap().PreserveReferences();
         CreateMap<ProductCategory, ProductCategoryDto>().ReverseMap().PreserveReferences();
+		CreateMap<ProductCategory, ProductCategoryForUpdateDto>().ReverseMap().PreserveReferences();
     }
 }
