c4c23b8b40947ff0f28fef5153d32ba3fcd4eddc
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-12-06 00:31:13 +0100

    add new productRepository

---
+++ src/Library/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Library.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Repositories.Interfaces;
+
+public interface IProductRepository
+{
+    IEnumerable<Product> Products { get; set; }
+
+    Task AddProductAsync(Product product);
+
+    Task<bool> DeleteProductAsync(int productId);
+
+    Task<Product?> GetProductAsync(int productId);
+
+    Task<List<Product>> GetAllProductsAsync();
+
+    Task<int> SaveChangesAsync();
+}
---
+++ src/Library/Repositories/ProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using Library.DataPersistence;
+using Library.Entities;
+using Library.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public IEnumerable<Product> Products { get; set; }
+
+    public ProductRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        Products = _dbContext.Products;
+    }
+
+    public async Task AddProductAsync(Product product)
+    {
+       await _dbContext.Products.AddAsync(product);
+    }
+
+    public async Task<bool> DeleteProductAsync(int productId)
+    {
+       var product = await _dbContext.Products.FindAsync(productId);
+       if(product is null)
+       {
+            return false;
+       }
+        _dbContext.Products.Remove(product);
+        return true;
+    }
+
+    public async Task<Product?> GetProductAsync(int productId)
+    {
+        return await _dbContext.Products.FindAsync(productId);
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<List<Product>> GetAllProductsAsync()
+    {
+        var products = await _dbContext.Products.ToListAsync();
+        return products;
+    }
+}
