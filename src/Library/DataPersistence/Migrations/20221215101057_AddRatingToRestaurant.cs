e3dd0ff636aaf77c446fffc79c3022aedeac93fa
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-12-15 12:01:42 +0100

    add rating to entity and database

---
+++ src/Library/DataPersistence/Migrations/20221215101057_AddRatingToRestaurant.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Migrations
+{
+    public partial class AddRatingToRestaurant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Rating",
+                table: "Restaurants",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Restaurants");
+        }
+    }
+}
