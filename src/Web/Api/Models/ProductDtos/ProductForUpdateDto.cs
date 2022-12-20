54469d47451d3b726a52a7b09d05087b4ca12269
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-12-20 20:14:47 +0100

    add restaurantId

--- src/Web/Api/Models/ProductDtos/ProductForUpdateDto.cs
+++ src/Web/Api/Models/ProductDtos/ProductForUpdateDto.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using Web.Api.Models.RestaurantDtos;
+using System.Reflection;
 
 namespace Web.Api.Models.ProductDtos;
 
@@ -21,6 +22,9 @@ public class ProductForUpdateDto
 
     [Required]
     public int ProductCategoryId { get; set; }
+
+    [Required]
+    public int RestaurantId { get; set; }
 
     [Required]
     public string ImageUrl { get; set; } = string.Empty;
