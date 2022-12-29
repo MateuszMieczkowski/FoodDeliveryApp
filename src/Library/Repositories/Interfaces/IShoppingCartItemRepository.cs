c50bb431bbd094a5a12142c2cd52eeb5050be9f4
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2022-12-29 13:50:35 +0100

    add shoppingCartItem repository and interface

---
+++ src/Library/Repositories/Interfaces/IShoppingCartItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.Entities;
+
+namespace Library.Repositories.Interfaces;
+
+public interface IShoppingCartItemRepository
+{
+    Task AddShoppingCartItemAsync(ShoppingCartItem shoppingCartItem);
+
+    void DeleteShoppingCartItem (ShoppingCartItem shoppingCartItem);
+
+    List<ShoppingCartItem> GetShoppingCartItems(Guid shoppingCartId);
+
+    Task<int> SaveChangesAsync();
+}
---
+++ src/Library/Repositories/ShoppingCartItemRepository.cs
@@ -0,0 +1,39 @@
+﻿using Library.DataPersistence;
+using Library.Entities;
+using Library.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Repositories;
+
+public class ShoppingCartItemRepository : IShoppingCartItemRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public ShoppingCartItemRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddShoppingCartItemAsync(ShoppingCartItem shoppingCartItem)
+    {
+        await _dbContext.ShoppingCartItems.AddAsync(shoppingCartItem);
+    }
+
+    public void DeleteShoppingCartItem(ShoppingCartItem shoppingCartItem)
+    {
+        _dbContext.ShoppingCartItems.Remove(shoppingCartItem);
+    }
+
+    public List<ShoppingCartItem> GetShoppingCartItems(Guid shoppingCartId)
+    {
+        var shoppingCartItems = _dbContext.ShoppingCartItems.Include(r => r.Product)
+                                                            .Where(r => r.ShoppingCartId.Equals(shoppingCartId))
+                                                            .ToList();
+        return shoppingCartItems;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+       return await _dbContext.SaveChangesAsync();
+    }
+}
