cc5bab89a073b9e8bde1b623ede3c609b6ca53a4
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2023-01-16 23:42:49 +0100

    Add IOrderRepository with implementation

---
+++ src/Library/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using Library.Entities;
+
+namespace Library.Repositories.Interfaces;
+
+public interface IOrderRepository
+{
+    IQueryable<Order> Orders { get; set; }
+
+    Task<List<Order>> GetAllOrders(Order order);
+
+    Task<List<Order>> GetUserOrders(Guid userId);
+
+    Task<List<Order>> GetRestaurantOrders(int restaurantId);
+
+    Task AddOrderAsync(Order order);
+
+    void DeleteOrder(Order order);
+
+    Task<Order?> GetOrderAsync(Guid orderId);
+
+    Task<int> SaveChangesAsync();
+}
---
+++ src/Library/Repositories/OrderRepository.cs
@@ -0,0 +1,54 @@
+﻿using Library.DataPersistence;
+using Library.Entities;
+using Library.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public OrderRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        Orders = dbContext.Orders;
+    }
+
+    public IQueryable<Order> Orders { get; set; }
+
+    public async Task AddOrderAsync(Order order)
+    {
+        await _dbContext.Orders.AddAsync(order);
+    }
+
+    public void DeleteOrder(Order order)
+    {
+        _dbContext.Orders.Remove(order);
+    }
+
+    public async Task<List<Order>> GetAllOrders(Order order)
+    {
+        return await _dbContext.Orders.ToListAsync();
+    }
+
+    public async Task<Order?> GetOrderAsync(Guid orderId)
+    {
+        return await _dbContext.Orders.FindAsync(orderId);
+    }
+
+    public async Task<List<Order>> GetRestaurantOrders(int restaurantId)
+    {
+        return await _dbContext.Orders.Where(x => x.RestaurantId == restaurantId).ToListAsync();
+    }
+
+    public async Task<List<Order>> GetUserOrders(Guid userId)
+    {
+        return await _dbContext.Orders.Where(x => x.UserId == userId).ToListAsync();
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _dbContext.SaveChangesAsync();
+    }
+}
