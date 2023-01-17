2d43e190917d976ac3177b8030a5c00f6eeaf1ab
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2023-01-17 13:24:18 +0100

    Custom authorization on user

---
+++ src/Library/Authorization/AccountOwnerRequirement.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Library.Authorization;
+
+public class AccountOwnerRequirement : IAuthorizationRequirement
+{
+    public Guid UserId { get; }
+	public AccountOwnerRequirement(Guid userId)
+	{
+		UserId = userId;
+	}
+}
---
+++ src/Library/Authorization/AccountOwnerRequirementHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace Library.Authorization;
+
+public class AccountOwnerRequirementHandler : AuthorizationHandler<AccountOwnerRequirement>
+{
+    protected async override Task HandleRequirementAsync(AuthorizationHandlerContext context, AccountOwnerRequirement requirement)
+    {
+        await Task.CompletedTask;
+
+        if (context.User.IsInRole("admin"))
+        {
+            context.Succeed(requirement);
+            return;
+        }
+        var nameIdentifire = context.User.FindFirst(x => x.Type == ClaimTypes.NameIdentifier);
+  
+        if(nameIdentifire?.Value == requirement.UserId.ToString())
+        {
+            context.Succeed(requirement);
+        }
+    }
+}
