97378ec595d40a5cda401bee1b2e27ea52ef9e9e
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2023-01-17 13:24:38 +0100

    Removed resource and add restaurantId

--- src/Library/Authorization/RestaurantManagerRequirement.cs
+++ src/Library/Authorization/RestaurantManagerRequirement.cs
@@ -4,5 +4,9 @@ namespace Library.Authorization;
 
 public class RestaurantManagerRequirement : IAuthorizationRequirement
 {
-    
+    public int RestaurantId { get; }
+	public RestaurantManagerRequirement(int restaurantId)
+	{
+		RestaurantId = restaurantId;
+	}
 }
--- src/Library/Authorization/RestaurantManagerRequirementHandler.cs
+++ src/Library/Authorization/RestaurantManagerRequirementHandler.cs
@@ -3,26 +3,24 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Library.Authorization;
 
-public class RestaurantManagerRequirementHandler : AuthorizationHandler<RestaurantManagerRequirement, Restaurant>
+public class RestaurantManagerRequirementHandler : AuthorizationHandler<RestaurantManagerRequirement>
 {
-    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, RestaurantManagerRequirement requirement,
-        Restaurant resource)
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, RestaurantManagerRequirement requirement)
     {
         await Task.CompletedTask;
         if (context.User.IsInRole("admin"))
         {
             context.Succeed(requirement);
+            return;
         }
 
         if (context.User.IsInRole("manager"))
         {
             var managedRestaurantId = context.User.FindFirst(c=> c.Type == "ManagedRestaurantId")?.Value;
-            if (managedRestaurantId == resource.Id.ToString())
+            if (managedRestaurantId == requirement.RestaurantId.ToString())
             {
                 context.Succeed(requirement);
             }
         }
-
-       
     }
 }
