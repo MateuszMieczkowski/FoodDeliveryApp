009c1b966a05b9e198401406acb77ec92af9b88d
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2023-03-26 20:25:21 +0200

    Add migrations

---
+++ src/Library/Migrations/20230215183856_AddDiscountAndRemoveRestaurantIdFromShoppingCartItems.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Migrations
+{
+    public partial class AddDiscountAndRemoveRestaurantIdFromShoppingCartItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartItems_Restaurants_RestaurantId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingCartItems_RestaurantId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DiscountId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Discount",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DiscountCode = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Percentage = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discount", x => x.Id);
+                    table.CheckConstraint("Percentage_Check", "Percentage BETWEEN 0.0 AND 1.0");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_DiscountId",
+                table: "Orders",
+                column: "DiscountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Discount_DiscountId",
+                table: "Orders",
+                column: "DiscountId",
+                principalTable: "Discount",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Discount_DiscountId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Discount");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_DiscountId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RestaurantId",
+                table: "ShoppingCartItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCartItems_RestaurantId",
+                table: "ShoppingCartItems",
+                column: "RestaurantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartItems_Restaurants_RestaurantId",
+                table: "ShoppingCartItems",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ src/Library/Migrations/20230215184926_ChangeDiscountTableNameToDiscounts.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Migrations
+{
+    public partial class ChangeDiscountTableNameToDiscounts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Discount_DiscountId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Discount",
+                table: "Discount");
+
+            migrationBuilder.RenameTable(
+                name: "Discount",
+                newName: "Discounts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Discounts",
+                table: "Discounts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Discounts_DiscountId",
+                table: "Orders",
+                column: "DiscountId",
+                principalTable: "Discounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Discounts_DiscountId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Discounts",
+                table: "Discounts");
+
+            migrationBuilder.RenameTable(
+                name: "Discounts",
+                newName: "Discount");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Discount",
+                table: "Discount",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Discount_DiscountId",
+                table: "Orders",
+                column: "DiscountId",
+                principalTable: "Discount",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
