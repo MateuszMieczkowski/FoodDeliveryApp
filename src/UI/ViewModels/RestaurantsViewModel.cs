bf51ca9f8614372852267d636eaedd8f55a84338
Mateusz Mieczkowski <82804@student.pb.edu.pl>
2023-03-27 21:37:53 +0200

    add restaurants view model

---
+++ src/UI/ViewModels/RestaurantsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using UI.API.Responses;
+
+namespace UI.ViewModels;
+
+public class RestaurantsViewModel
+{
+    public int PageSize { get; set; } = 10;
+
+    public string? SearchQuery { get; set; }
+
+    public string? City { get; set; }
+
+    public string? RestaurantCategoryName { get; set; }
+
+    public PagedResult<Restaurant>? PagedResult { get; set; }
+}
